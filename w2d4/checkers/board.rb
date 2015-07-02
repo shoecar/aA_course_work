11a73bd99a071fc540757bab5f9e3e1172ceff6b
Tad Schukar <tschukar@gmail.com>
2015-07-02 13:07:19 -0700

    set up display for game

--- w2d4/checkers/board.rb
+++ w2d4/checkers/board.rb
@@ -1,10 +1,18 @@
-require 'piece.rb'
+require_relative 'piece.rb'
+require 'colorize'
 
 class Board
+  attr_accessor :cursor
   attr_reader :grid
 
   def initialize
     @grid = Array.new(10) { Array.new(10) { EmptySquare.new } }
+    populate
+    @cursor = [5,5]
+  end
+
+  def inspect
+    {}
   end
 
   def [](position)
@@ -20,24 +28,40 @@ class Board
   def populate
     (0..9).each_with_index do |row|
       (0..9).each_with_index do |col|
-        if row.between?(0..2) && (row - col) % 2 > 0
-          Piece.new(:red, [row, col], self)
-        elsif row.between(7..9) && (row - col) % 2 > 0
-          Piece.new(:white, [row, col], self)
+        if row < 3 && (row - col) % 2 > 0
+          self[[row, col]] = Piece.new(:red, [row, col], self)
+        elsif row > 6 && (row - col) % 2 > 0
+          self[[row, col]] = Piece.new(:white, [row, col], self)
         end
+      end
     end
   end
 
   def display
-    grid.each do |row|
-      row.each do |piece|
-        if piece.empty?
-          print "_"
+    grid.each_with_index do |row, row_idx|
+      row.each_with_index do |piece, col_idx|
+        if cursor == [row_idx, col_idx]
+          print " #{piece} ".colorize(:background => :yellow)
+        elsif (row_idx - col_idx) % 2 > 0
+          print " #{piece} ".colorize(:background => :black)
         else
-          print piece
+          print " #{piece} ".colorize(:background => :white)
         end
       end
+      puts
     end
-    puts
   end
+
+  def is_enemy?(position, color)
+    self[position].color != :e || self[position].color != color
+  end
+
+  def no_object?(position, color)
+    row, col = position
+    row.between?(0, 9) && col.between?(0, 9) && self[position].color != color
+  end
+
 end
+
+b = Board.new
+b.display
--- w2d4/checkers/piece.rb
+++ w2d4/checkers/piece.rb
@@ -1,8 +1,11 @@
 class Piece
-  WHITE_MOVES = [[1, -1], [1, 1]]
-  RED_MOVES =  [[-1, -1], [-1, 1]]
+  WHITE_MOVES = [[-1, 1], [-1, -1]]
+  RED_MOVES =  [[1, -1], [1, 1]]
+  WHITE_JUMPS = [[-2, 2], [-2, -2]]
+  RED_JUMPS =  [[2, -2], [2, 2]]
 
-  attr_reader :color, :position, :king, :board
+  attr_reader :color, :king, :board
+  attr_accessor :position
 
   def initialize(color, position, board)
     @color = color
@@ -12,24 +15,51 @@ class Piece
     @board = board
   end
 
-  def move_diffs
-    if king?
-      WHITE_MOVES + RED_MOVES
+  def move_diffs(type)
+    if type == :move
+      if king?
+        WHITE_MOVES + RED_MOVES
+      else
+        color == :white ? WHITE_MOVES : RED_MOVES
+      end
     else
-      color == :w ? WHITE_MOVES : RED_MOVES
+      if king?
+        WHITE_JUMPS + RED_JUMPS
+      else
+        color == :white ? WHITE_JUMPS : RED_JUMPS
+      end
     end
   end
 
-  def perfrom_move(to_position)
-    possible = move_diffs.map do |move|
-      combine_positions(position, move) == to_position
+  def perform_move(to_position)
+    if position_viable?(to_position, :move)
+      board[position] = EmptySquare.new
+      board[to_position] = self
+      position = to_position
     end
+  end
 
+  def perform_jump(to_position)
+    if position_viable?(to_position, :jump) &&
+        board.is_enemy?(between_positions(to_position, position))
 
+    end
+  end
+
+  def position_viable?(to_position, symbol)
+    move_diffs(symbol).any? do |move|
+      combine_positions(position, move) == to_position &&
+          board.no_object?(to_position, color)
+    end
   end
 
   def combine_positions(first, second)
     [first[0] + second[0], first[1] + second[1]]
+  end
+
+  def between_positions(first, second)
+    row, col = combine_positions(first, second)
+    [row / 2, col / 2]
   end
 
   def king?
@@ -39,13 +69,30 @@ class Piece
   def empty?
     false
   end
+
+  def to_s
+    if color == :white
+      "⚪"
+    else
+      "⚫"
+    end
+  end
+
 end
 
 class EmptySquare
+  attr_reader :color
+
   def initialize
+    @color = :e
   end
 
   def empty?
     true
   end
+
+  def to_s
+    " "
+  end
+
 end
